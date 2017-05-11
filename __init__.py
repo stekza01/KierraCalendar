a0d638251d89ce6b11e96bf648bfa1b4198b18e0
stekza01 <stekza01@luther.edu>
2017-05-10 21:42:05 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,15 +10,12 @@ from wtforms import TextField, BooleanField, PasswordField, TextAreaField, Strin
 from flask import Flask, render_template, flash, request, url_for
 from flask.ext.heroku import Heroku
 from flask.ext.login import LoginManager, login_user, logout_user, current_user, login_required
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 app.secret_key = '\xa7h\xb7)\xce\xce\x8c\xa9`\xce\xa1\xdb\x9b1;F\x12c\xb2\xe9\xe7\xe8\x92\xe2'
 app.debug = True
 
 heroku = Heroku(app)
-db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 
 
